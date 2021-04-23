619e8edce2a283f90e72c714ae6deb7c6ae41a62
Ujjwalsharma2210 <ujjwalsharma2210@gmail.com>
2021-04-23 22:04:38 +0530

    functionalitiy improved (actually working)

--- mainFile.py
+++ mainFile.py
@@ -12,35 +12,104 @@ from kivy.properties import ObjectProperty
 import json
 
 
+def write_json(data, filename='credentials.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
 class LoginScreen(Screen):
+
+    def clearCredentials(self, usern, passw):
+        usern = ""
+        passw = ""
+
+    def logIn(self):
+
+        self.manager.current = "AccountPage"
+
+        # clears the text entered by user
+        self.identification.text = ""
+        self.password.text = ""
+
+    def addUser(self, username, password):
+
+        with open('credentials.json', 'r') as json_file:
+            data = json.load(json_file)
+
+        isUser = True
+
+        while isUser:
+            for user in data['userAccounts']:
+                if user['username'] == username:
+                    print("____User Found____")
+
+                    if user['password'] == password:
+                        print("Correct password entered!!")
+                        isUser = False
+                        self.logIn()
+                        break
+                    else:
+                        print("Incorrect username or password!!")
+
+                else:
+                    continue
+
+            break
+
     def onLoginClick(self):
         print("_______**TRIED TO LOGIN**_______")
-        if self.ids["identification"].text == "username" and self.ids["password"].text == "password":
-            self.manager.current = "AccountPage"
-            print("Logger ID : {0}".format(self.ids["identification"].text))
-        else:
-            self.identification.text = ""
-            self.password.text = ""
+
+        # checks if the entered username and password are present in json file
+        self.addUser(self.identification.text, self.password.text)
+
+        # with open('credentials.json', 'r') as json_file:
+        #     data = json.load(json_file)
+
+        # isUser = True
+        # while isUser:
+        #     for user in data['userAccounts']:
+        #         if user['username'] == self.identification.text:
+        #             print("____User Found____")
+        #
+        #             if user['password'] == self.password.text:
+        #                 print("Correct password entered!!")
+        #                 self.logIn()
+        #                 isUser = False
+        #             else:
+        #                 print("Incorrect password!!")
+        #
+        #         else:
+        #             print("____User Not Found____")
+        #             isUser = False
 
 
 class AccountScreen(Screen):
-    pass
+    def logOut(self):
+        self.manager.current = "LoginPage"
 
 
 class AddAccountScreen(Screen):
     def onAddAccountClick(self):
-        data = {}
-        with open('credentials.json') as f:
-            data = json.load(f)
 
-        # data['user'] = self.ids["newIdentification"].text
-        # data['password'] = self.ids["newPassword"].text
+        # Exit if either id or password is empty
+        if self.ids["newIdentification"].text == "" or self.ids["newPassword"].text == "":
+            self.manager.current = "LoginPage"
+        else:
+            # adding new id and password to json file
+            with open('credentials.json') as json_file:
+                data = json.load(json_file)
 
-        with open('credentials.json', 'a') as f:
-            json.dump(data, f)
+                temp = data['userAccounts']
 
-        self.newIdentification.text = ""
-        self.newPassword.text = ""
+                y = {"username": self.ids["newIdentification"].text,
+                     "password": self.ids["newPassword"].text}
+
+                temp.append(y)
+
+            write_json(data)
+
+            self.newIdentification.text = ""
+            self.newPassword.text = ""
 
         self.manager.current = "LoginPage"
 
